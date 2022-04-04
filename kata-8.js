4a2442c218b0a6065fb81450d0ee293ae8117ffa
Mai-ELhajeen <maielhajeen2001@gmail.com>
2022-04-05 01:09:32 +0300

    adding answer question for kata-8-16

--- kata-8.js
+++ kata-8.js
@@ -319,3 +319,23 @@ I'm new to coding and now I want to get the sum of two arrays...actually the sum
 I'll appreciate for your help.
 P.S. Each array includes only integer numbers. Output is a number too.
 */
+
+function arrayPlusArray (arr1, arr2){
+    let sum = 0;
+    newArray =  arr1.concat(arr2)
+    for(let i of newArray){
+        sum += i;
+    }
+    return sum
+}
+console.log(arrayPlusArray ([1, 2, 3] , [4, 5, 6]))
+
+arrayPlusArray = (arr1, arr2) => {
+    let sum = 0;
+    newArray =  arr1.concat(arr2)
+    for(let i of newArray){
+        sum += i;
+    }
+    return sum
+}
+console.log(arrayPlusArray ([1, 2, 3] , [4, 5, 6]))
