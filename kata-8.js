e5ddb0f8cf59870fd166e287ab1914c4bff2050f
Mai-ELhajeen <maielhajeen2001@gmail.com>
2022-04-04 22:35:16 +0300

    aading question title and question for kata-8-3

--- kata-8.js
+++ kata-8.js
@@ -17,7 +17,16 @@ The first character in the string is always a number.
 /*
 Implement a function which multiplies two numbers.
 */
-function multiply (a, b){
-    return a * b
-}
-console.log(multiply(5, 9))
+
+// function multiply (a, b){
+//     return a * b
+// }
+// console.log(multiply(5, 9))
+
+// ? If you can't sleep, just count sheep!!
+/*
+If you can't sleep, just count sheep!!
+Task:
+Given a non-negative integer, 3 for example,
+return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+*/
