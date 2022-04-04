7d1ce74bd28498b6ddb1074c8d422b41f3da8ad8
Mai-ELhajeen <maielhajeen2001@gmail.com>
2022-04-05 00:22:15 +0300

    adding answer question for kata-8-12

--- kata-8.js
+++ kata-8.js
@@ -237,3 +237,29 @@ You get an array of numbers, return the sum of all of the positives ones.
 Example [1,-4,7,12] => 1 + 7 + 12 = 20
 Note: if there is nothing to sum, the sum is default to 0.
 */
+
+function positiveSum (arr){
+    // console.log(arr)
+    let sum = 0;
+    for(let i=0; i<arr.length; i++){
+        // console.log(arr[i])
+        if(arr[i] > 0){
+            // console.log(arr[i])
+            sum += arr[i]
+        }
+    }
+    return sum
+}
+console.log(positiveSum([1,-4,7,12]))
+
+
+positiveSum = (arr) => {
+    let sum = 0;
+    for(let i of arr){
+        if(i > 0){
+            sum += i
+        }
+    }
+    return sum;
+}
+console.log(positiveSum([1,-4,7,12]))
