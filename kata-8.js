3d7d6ee54d4d22c1118bd8480fb773170f433128
Mai-ELhajeen <maielhajeen2001@gmail.com>
2022-04-04 22:43:31 +0300

    adding question title and question for kata-8-4

--- kata-8.js
+++ kata-8.js
@@ -23,6 +23,8 @@ Implement a function which multiplies two numbers.
 // }
 // console.log(multiply(5, 9))
 
+// ************************************************************************************************************************
+
 // ? If you can't sleep, just count sheep!!
 /*
 If you can't sleep, just count sheep!!
@@ -31,10 +33,17 @@ Given a non-negative integer, 3 for example,
 return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
 */
 
-var countSheep = function (num){
-    let count = '';
-    for(let i=1; i<=num; i++){
-        count = count + `${[i]} sheep...`
-    }
-    return count;
-}
+// var countSheep = function (num){
+//     let count = '';
+//     for(let i=1; i<=num; i++){
+//         count = count + `${[i]} sheep...`
+//     }
+//     return count;
+// }
+
+// ************************************************************************************************************************
+// ? Convert a string to an array
+/*
+Write a function to split a string and convert it into an array of words.
+output: * "Robin Singh" ==> ["Robin", "Singh"]
+*/
