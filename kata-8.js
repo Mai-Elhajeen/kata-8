563a48860e92ed14a92ba59caafa383a524873b0
Mai-ELhajeen <maielhajeen2001@gmail.com>
2022-04-04 23:03:25 +0300

    adding question title and question for kata-8-5

--- kata-8.js
+++ kata-8.js
@@ -51,3 +51,12 @@ function stringToArray(string){
     // code code code
     return string.split(' ');
 }
+
+// ************************************************************************************************************************
+
+// ? Convert a Number to a String!
+/*
+We need a function that can transform a number into a string.
+What ways of achieving this do you know?
+output : 123 --> "123" || 999 --> "999"
+*/ 
