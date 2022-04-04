aa6bf4d02107fdd76249bc6171d1506b0095df75
Mai-ELhajeen <maielhajeen2001@gmail.com>
2022-04-04 23:14:47 +0300

    adding answer question for kata-8-6

--- kata-8.js
+++ kata-8.js
@@ -71,3 +71,7 @@ output : 123 --> "123" || 999 --> "999"
 /*
 Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
 */
+function boolToWord( bool ){
+    return bool === true ? 'Yes' : 'No';
+}
+
