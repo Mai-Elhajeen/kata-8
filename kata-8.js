92b275dd89b992a51a6f2f1635ae3103eb6c4c59
Mai-ELhajeen <maielhajeen2001@gmail.com>
2022-04-04 23:51:48 +0300

    adding question title and question for kata-8-9

--- kata-8.js
+++ kata-8.js
@@ -102,7 +102,21 @@ Example: (Input1, Input2 -->Output)
 "-5", "3" --> "-2"
 */
 
-function sumStr(a,b) {
-    const total = Number(a) + Number(b);
-    return total.toString()
-}
+// function sumStr(a,b) {
+//     const total = Number(a) + Number(b);
+//     return total.toString()
+// }
+
+// ************************************************************************************************************************
+
+// ? Exclamation marks series #1: Remove an exclamation mark from the end of string
+/* 
+Remove an exclamation mark from the end of a string. 
+For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+remove("Hi!") === "Hi"
+remove("Hi!!!") === "Hi!!"
+remove("!Hi") === "!Hi"
+remove("!Hi!") === "!Hi"
+remove("Hi! Hi!") === "Hi! Hi"
+remove("Hi") === "Hi"
+*/
