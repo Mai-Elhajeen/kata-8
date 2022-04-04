141f8b3aa8d7d8069b1ef3f2b1bc72540c683c26
Mai-ELhajeen <maielhajeen2001@gmail.com>
2022-04-05 00:04:26 +0300

    adding answer question for kata-8-10

--- kata-8.js
+++ kata-8.js
@@ -155,3 +155,40 @@ Example
 For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
 */ 
 
+function countPositivesSumNegatives (input) {
+    let count = 0;
+    let sum = 0;
+    if (input != null){
+        for (let i = 0; i < input.length; i++) {
+            if(input[i] > 0){
+                count++
+            }else if(input[i] < 0){
+                sum += input[i]
+            }
+        }
+        if(count == 0 && sum == 0){
+            return []
+        }else{
+            return [count, sum]
+        }
+    }else{
+        return []
+    }
+}
+// countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15])
+console.log(countPositivesSumNegatives([0, 0]));
+
+function countPositivesSumNegatives(input) {
+    let arr = []
+    if (input !== null){
+        let positiveArr = input.filter(e => e>0).length
+        let negativeArr = input.filter(x => x<0).reduce((c,n)=> c+n,0)
+        if(positiveArr != [0] ||negativeArr !=[0]){
+            arr.push(positiveArr,negativeArr)
+        }
+        return arr
+    } else{
+        return []
+    }
+}
+console.log(countPositivesSumNegatives([0, 0]));
