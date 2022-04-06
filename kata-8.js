87e858df3769e7b141f7c25c3f27c7daa6d90a2e
Mai-ELhajeen <maielhajeen2001@gmail.com>
2022-04-06 04:04:10 +0300

    adding question title and question for kata-8-19

--- kata-8.js
+++ kata-8.js
@@ -376,8 +376,23 @@ your program should return, "Hello, <name> how are you doing today?".
 [Make sure you type the exact thing I wrote or the program may not execute properly]
 */
 
-function greet(name){
-    //your code here
-    return `Hello, ${name} how are you doing today?`
-}
-console.log(greet('mai')) 
+// function greet(name){
+//     //your code here
+//     return `Hello, ${name} how are you doing today?`
+// }
+// console.log(greet('mai')) 
+
+// ************************************************************************************************************************
+
+// ? Find Maximum and Minimum Values of a List
+/*
+Your task is to make two functions (max and min, or maximum and minimum, etc.,
+depending on the language) that receive a list of integers as input and return, respectively, 
+the largest and lowest number in that list.
+
+Examples (Input -> Output)
+* [4,6,2,1,9,63,-134,566]         -> max = 566, min = -134
+* [-52, 56, 30, 29, -54, 0, -110] -> min = -110, max = 56
+* [42, 54, 65, 87, 0]             -> min = 0, max = 87
+* [5]                             -> min = 5, max = 5
+*/
