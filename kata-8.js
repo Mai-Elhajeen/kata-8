16ccbfc07ee32cfffab4565245558911e1b43138
Mai-ELhajeen <maielhajeen2001@gmail.com>
2022-05-19 17:38:57 +0300

    adding answer question for kata-8-27

--- kata-8.js
+++ kata-8.js
@@ -541,3 +541,12 @@ Examples
 In this simple assignment you are given a number and have to make it negative. 
 But maybe the number is already negative?
 */
+function makeNegative(num) {
+    // Code?
+    if (num > 0){
+        const newNum = '-' + num
+        return Number(newNum)
+    }else{
+        return num
+    }
+}
