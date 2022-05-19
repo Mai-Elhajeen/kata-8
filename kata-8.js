56b7b9b9e554643e2feb564ccd96f33f5209ab31
Mai-ELhajeen <maielhajeen2001@gmail.com>
2022-05-19 15:55:59 +0300

    adding content the question for kata-8-25

--- kata-8.js
+++ kata-8.js
@@ -505,3 +505,7 @@ All values will be positive integers or floats, or zero.
 // ************************************************************************************************************************
 
 // ? Counting sheep...
+/*
+Consider an array/list of sheep where some sheep may be missing from their place. 
+We need a function that counts the number of sheep present in the array (true means present).
+*/
