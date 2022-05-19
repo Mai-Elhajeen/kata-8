0b18cf8b22a117e42b77f4800e87bf5a6149df9b
Mai-ELhajeen <maielhajeen2001@gmail.com>
2022-05-19 17:17:18 +0300

    adding answer question for kata-8-25

--- kata-8.js
+++ kata-8.js
@@ -509,3 +509,13 @@ All values will be positive integers or floats, or zero.
 Consider an array/list of sheep where some sheep may be missing from their place. 
 We need a function that counts the number of sheep present in the array (true means present).
 */
+function countSheeps(arrayOfSheep) {
+    // TODO May the force be with you
+    let sum = 0
+    for(let i =0; i < arrayOfSheep.length; i++){
+        if (arrayOfSheep[i] === true){
+            sum ++
+        }
+    }
+    return sum
+}
